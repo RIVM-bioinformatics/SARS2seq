eb84fdc62cac39d49da72967e1fd70811ce91f88
Florian Zwagemaker <florian@niqex.com>
2021-05-11 12:54:11 +0200

    formatting and sorted imports

--- SARS2seq/workflow/scripts/AA_extract.py
+++ SARS2seq/workflow/scripts/AA_extract.py
@@ -1,7 +1,8 @@
+import sys
+
 import gffpandas.gffpandas as gffpd
 from Bio import SeqIO
 from Bio.Seq import Seq
-import sys
 
 nucleotides, features, currentorf, samplename, cov, outdir = sys.argv[1:]
 
@@ -9,19 +10,16 @@ nucleotides, features, currentorf, samplename, cov, outdir = sys.argv[1:]
 seq = str(SeqIO.read(nucleotides, "fasta").seq)
 
 ## read features
-gffdict = gffpd.read_gff3(features).df.to_dict(orient='index')
+gffdict = gffpd.read_gff3(features).df.to_dict(orient="index")
 
 for k in gffdict.keys():
-    if currentorf == str(gffdict[k].get('attributes').split(';')[1].split('=')[-1]):
-        orfname = str(gffdict[k].get('attributes').split(';')[1].split('=')[-1])
-        start = int(gffdict[k].get('start'))
-        end = int(gffdict[k].get('end'))
-        
-        sliced_seq = seq[start-1:end].replace('-', '')
+    if currentorf == str(gffdict[k].get("attributes").split(";")[1].split("=")[-1]):
+        orfname = str(gffdict[k].get("attributes").split(";")[1].split("=")[-1])
+        start = int(gffdict[k].get("start"))
+        end = int(gffdict[k].get("end"))
+
+        sliced_seq = seq[start - 1 : end].replace("-", "")
         AminoAcids = Seq(sliced_seq).translate(to_stop=True)
-        
+
         with open(f"{outdir}/{orfname}/{samplename}_{cov}.fa", "w") as out:
-            out.write(
-                f">{samplename}_ORF-{orfname}_cov_{cov}\n"
-                f"{AminoAcids}\n"
-            )
+            out.write(f">{samplename}_ORF-{orfname}_cov_{cov}\n" f"{AminoAcids}\n")
--- SARS2seq/workflow/scripts/typingagg.py
+++ SARS2seq/workflow/scripts/typingagg.py
@@ -1,21 +1,22 @@
-import pandas as pd
 import sys
 from datetime import date
 
+import pandas as pd
+
 samplename, nx_vs, pn_vs, nx_typ, pn_typ, outfile = sys.argv[1:]
 
-nextclade = pd.read_csv(nx_typ, sep=';').to_dict(orient='list')
-pangolin = pd.read_csv(pn_typ, sep=',').to_dict(orient='list')
+nextclade = pd.read_csv(nx_typ, sep=";").to_dict(orient="list")
+pangolin = pd.read_csv(pn_typ, sep=",").to_dict(orient="list")
 
 now = date.today().strftime("%d-%m-%Y")
 
-with open(nx_vs, 'r') as nf:
-    nx_version = nf.read().split(' ')[-1].rstrip()
+with open(nx_vs, "r") as nf:
+    nx_version = nf.read().split(" ")[-1].rstrip()
 
-with open(pn_vs, 'r') as pf:
-    pn_version = pf.read().split(' ')[-1].rstrip()
-    
-with open(outfile, 'w') as out:
+with open(pn_vs, "r") as pf:
+    pn_version = pf.read().split(" ")[-1].rstrip()
+
+with open(outfile, "w") as out:
     out.write(
         f"{samplename}\t{now}\t{pn_version}\t{nx_version}\t{pangolin.get('pangoLEARN_version')[0]}\t{pangolin.get('lineage')[0]}\t{nextclade.get('clade')[0]}\t{pangolin.get('status')[0]}\t{nextclade.get('qc.overallStatus')[0]}\n"
     )
